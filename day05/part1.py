f9c95cbc58c0076c3364e2c77703811058171e51
hadrian-reppas <hadrian@uchicago.edu>
2022-12-07 15:42:20 -0500

    reuse code on day 5

--- day05/part1.py
+++ day05/part1.py
@@ -7,12 +7,13 @@ for line in reversed(start.split("\n")):
         if c.isalpha():
             stacks[i].append(c)
 
-for line in moves.split("\n"):
-    n, start, end = map(int, line.split()[1::2])
-    for _ in range(n):
-        c = stacks[start - 1].pop()
-        stacks[end - 1].append(c)
+if __name__ == "__main__":
+    for line in moves.split("\n"):
+        n, start, end = map(int, line.split()[1::2])
+        for _ in range(n):
+            c = stacks[start - 1].pop()
+            stacks[end - 1].append(c)
 
-for stack in stacks:
-    print(stack.pop(), end="")
-print()
+    for stack in stacks:
+        print(stack.pop(), end="")
+    print()
--- day05/part2.py
+++ day05/part2.py
@@ -1,11 +1,4 @@
-start, moves = open("input.txt").read().split("\n\n")
-
-stacks = [[] for _ in range(9)]
-for line in reversed(start.split("\n")):
-    for i in range(9):
-        c = line[1 + 4 * i]
-        if c.isalpha():
-            stacks[i].append(c)
+from part1 import stacks, moves
 
 for line in moves.split("\n"):
     n, start, end = map(int, line.split()[1::2])
